888b133aec3ebda32a310772bf7c18e13954069f
Psychohistorian11 <cristianfrancoraigosa.com>
2025-08-30 21:11:12 -0500

    in menu settings my job is developer in this moment the configuration of the home content and the first that the user will see, doesnt matter that the user is not login in the system, for the moment is already the design of asidebar and navigation bar for differents tools, the next issue is developer to profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
-import { redirect } from "next/navigation";
+import { AppDesktopSidebar } from "@/components/sidebar/app-desktop-sidebar"
+import { AppMobileSidebar } from "@/components/sidebar/app-movil-sidebar"
+import { ModeToggle } from "@/components/ui/mode-toggle"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
 
-export default function Home() {
-  redirect("/login");
+export default function HomePage() {
+  return (
 
-  return null
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+
+      {/* Desktop Sidebar Navigation */}
+
+      <div className="hidden md:flex w-full">
+        <AppDesktopSidebar />
+        <SidebarInset >
+          <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4 w-full"
+            />
+          </header>
+        </SidebarInset>
+      </div>
+
+      {/* Mobile Bottom Navigation */}
+      <AppMobileSidebar />
+    </SidebarProvider>
+
+
+
+  )
 }
