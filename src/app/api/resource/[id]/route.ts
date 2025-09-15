3f751a5c22f7e467adf295ba6a1eea868c054f4a
Psychohistorian11 <cristianfrancoraigosa.com>
2025-09-14 23:33:08 -0500

    fix: deploy

--- src/app/api/resource/[id]/route.ts
+++ src/app/api/resource/[id]/route.ts
@@ -18,34 +18,4 @@ export async function GET(request: NextRequest, context: any) {
 }
 
 
-export async function DELETE(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const existingResource = await prismadb.resource.findUnique({
-      where: { id: params.id },
-    });
-
-    if (!existingResource) {
-      return NextResponse.json(
-        { message: "Resource not found" },
-        { status: 404 }
-      );
-    }
-
-    await prismadb.resource.delete({
-      where: { id: params.id },
-    });
-
-    return NextResponse.json({ message: "Resource deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting resource:", error);
-    return NextResponse.json(
-      { message: "Error deleting resource" },
-      { status: 500 }
-    );
-  }
-}
-
 
