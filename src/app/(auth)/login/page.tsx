55eb8e9681f8aa09c36e38f2328be1e4437ecd7f
Juanfer <110195163+Hidalgo87@users.noreply.github.com>
2025-11-06 08:41:22 -0500

    Fix/community crud (#23)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,20 @@
 import LogInForm from "@/components/auth/login-form";
 
 export default function LoginPage() {
-    return (
-        <div className="min-h-dvh flex flex-row">
-            <div className=" md:basis-2/3 hidden md:block pl-6 pt-6 pb-6 ">
-                <div className="dark:bg-white bg-black h-full rounded-lg flex flex-col justify-center items-center">
-                    <p className="dark:text-black text-white">Background image</p>
-                </div>
-
-            </div>
-            <div className=" md:basis-1/3 w-full p-8">
-                <LogInForm />
-            </div>
+  return (
+    <div className="min-h-dvh flex flex-row">
+      <div className=" md:basis-2/3 hidden md:block pl-6 pt-6 pb-6 ">
+        <div className="dark:bg-white bg-black h-full rounded-lg flex flex-col justify-center items-center">
+          <img
+            src="fondo.jpeg"
+            alt="Background"
+            className="object-cover w-full h-full"
+          />{" "}
         </div>
-    );
-}  
+      </div>
+      <div className=" md:basis-1/3 w-full p-8">
+        <LogInForm />
+      </div>
+    </div>
+  );
+}
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -5,7 +5,11 @@ export default function LoginPage() {
     <div className="min-h-dvh flex flex-row">
       <div className=" md:basis-2/3 hidden md:block pl-6 pt-6 pb-6 ">
         <div className="dark:bg-white bg-black h-full rounded-lg flex flex-col justify-center items-center">
-          <p className="dark:text-black text-white">Background image</p>
+          <img
+            src="fondo.jpeg"
+            alt="Background"
+            className="object-cover w-full h-full"
+          />
         </div>
       </div>
       <div className=" md:basis-1/3 w-full p-8">
